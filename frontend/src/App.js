7d0c5dbfb15c9c2583d51d71d15b90606d8eb3bb
k0sten <rjcnty@gmail.com>
2021-08-05 20:56:43 +0300

    await

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  const serv = axios.get('http://87.249.49.53:3001')
+  console.log(serv)
   return (
     <div className="App">
       <div>Сдесь будет проект 😀</div>
