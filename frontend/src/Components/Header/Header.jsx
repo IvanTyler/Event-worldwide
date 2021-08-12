541c15237c1d8bf0d50c603af50012339a8e29c3
Ivan <Ivan_567.92@mail.ru>
2021-08-12 18:12:05 +0300

    work navbar

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,18 +1,22 @@
 import style from './Header.module.css';
 import styleContainer from '../Container/container.module.css';
 import logo from './img/eww.png'
+import telegram from './img/telegram.png'
 
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../redux/actions/userAC';
-import { useEffect, useState } from 'react';
-
+import { useEffect, useRef, useState } from 'react';
 
 function Header() {
   const dispatch = useDispatch()
   const user = useSelector((state) => state.user.id);
   console.log(user);
+
+  const menuNav = useRef(null)
+
+  const [menuNavBar, setmenuNavBar] = useState(false)
 
   const logoutSession = () => {
     dispatch(logout(''))
@@ -22,6 +26,18 @@ function Header() {
   useEffect(() => {
 
   }, [location]);
+
+
+  const showNavbar = () => {
+    console.log(menuNav.current)
+    if (menuNavBar === false) {
+      menuNav.current.style.right = '0'
+      setmenuNavBar(true)
+    } else if (menuNavBar === true) {
+      menuNav.current.style.right = '-320px'
+      setmenuNavBar(false)
+    }
+  }
 
   return (
     <>
@@ -64,12 +80,14 @@ function Header() {
               <li className={style.headerMenuItem}>
                 {user ? (
                   <>
-                    <Link className={style.headerMenuLink} to="/personalArea">
-                      Личный кабинет
-                    </Link>
                     <Link onClick={() => logoutSession()} className={style.headerMenuLink} to="/logout">
                       Выйти
                     </Link>
+                    <div onClick={() => showNavbar()} class={style.headerSandwich}>
+                      <div class={style.sandwichItem}></div>
+                      <div class={style.sandwichItem}></div>
+                      <div class={style.sandwichItem}></div>
+                    </div>
                   </>
                 ) : (
                   <>
@@ -84,6 +102,36 @@ function Header() {
               </li>
             </ul>
           </nav>
+        </div>
+        <div ref={menuNav} className={style.menuNav}>
+          <div className={style.menuNavItem}>
+            <Link className={style.headerMenuLink} to="/personalArea">
+              Личный кабинет
+            </Link>
+          </div>
+          <div className={style.menuNavItem}>
+            <Link className={style.headerMenuLink} to="/search">
+              Поиск событий
+            </Link>
+          </div>
+          <div className={style.menuNavItem}>
+            <Link className={style.headerMenuLink} to="/quicksearch">
+              Быстрый поиск
+            </Link>
+          </div>
+          <div className={style.menuNavItem}>
+            <Link className={style.headerMenuLink} to="/events">
+              Мои события
+            </Link>
+          </div>
+          <div className={style.menuNavItem}>
+            <Link ClassName={style.headerMenuLink + ' ' + style.headerMenuLinkTelegram} to="/https://t.me/event_worldwideBot">
+              <div className={style.telegramBlock}>
+                <img className={style.telegramIcon} src={telegram} alt="telegram" />
+              </div>
+              <span className={style.telegramBot}>Телеграм бот</span>
+            </Link>
+          </div>
         </div>
       </header>
     </>
